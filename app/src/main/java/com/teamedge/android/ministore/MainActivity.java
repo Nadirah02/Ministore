fe5f04525f6d6392137ce06e0c02d6006e48f04a
Student <student@Admins-MacBook-Air.local>
2018-12-08 18:38:58 -0500

    Initial commit

---
+++ app/src/main/java/com/teamedge/android/ministore/MainActivity.java
@@ -0,0 +1,25 @@
+package com.teamedge.android.ministore;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void sumbitOrder(View, view)
+
+        Checkbox
+
+
+
+
+
+}
